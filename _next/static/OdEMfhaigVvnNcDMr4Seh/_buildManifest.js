23439ddfdfc704d4006e73b19de49d14f7ee2786
KakoozaJerry <abrahamkakooza@gmail.com>
2021-12-02 08:29:17 +0000

    Deploying to gh-pages from @ oscakampala/oscakampala.github.io@2e92fbbfca382c54033f5199ee9e0a5f7df6c9e4 🚀

--- _next/static/OdEMfhaigVvnNcDMr4Seh/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-62deebbb3a951485.js"],"/_error":["static/chunks/pages/_error-6679c1efb053c50e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
