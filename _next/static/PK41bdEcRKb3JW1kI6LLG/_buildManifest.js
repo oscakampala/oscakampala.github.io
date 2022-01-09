e6ddbd9c2a47942174b0565573648fbefbb0131c
KakoozaJerry <KakoozaJerry@users.noreply.github.com>
2022-01-09 07:12:02 +0000

    Deploying to gh-pages from @ oscakampala/oscakampala.github.io@f8dc83237ac4d0b6281edd6a0a693673f2bc9f23 🚀

--- _next/static/PK41bdEcRKb3JW1kI6LLG/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-5a5153f17ab9fa23.js"],"/_error":["static/chunks/pages/_error-6679c1efb053c50e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/ox0jQ5Lz3q4Fa9B-1peol/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/366-74e86a3b2e626aea.js","static/chunks/pages/index-ecc5cc8cd5369294.js"],"/_error":["static/chunks/pages/_error-6679c1efb053c50e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
