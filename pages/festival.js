97f3e8b691496c22cabc07fb1dea10a28b28eebf
Abraham Jerry Kakooza <abrahamkakooza@gmail.com>
2022-03-01 14:00:14 +0300

    Done

--- pages/festival.js
+++ pages/festival.js
@@ -1,19 +1,23 @@
-import { Button } from '@mui/material';
+import { Button, Container } from '@mui/material';
 import * as React from 'react';
 
 export default function Festival() {
     return (
-            <>
-                <h1>THE OPEN SOURCE FESTIVAL 2022 KAMPALA EDITION</h1>
+            
+            <Container>
+                <HTMLHeadingElement>
+                    THE OPEN SOURCE FESTIVAL 2022 KAMPALA EDITION
+                </HTMLHeadingElement>
 
                 <h3>Date : 9th April 2022</h3>
                 <h4>Time : 10 AM to 5 PM</h4>
 
                 <h2>Concept note</h2>
+                
                 <p>Open collaboration often allows communities to achieve more than their members can do alone. 
                 This, in turn, accelerates innovation and is one of the reasons all the tech giants such as Microsoft,
-                 Google, Meta, are supporting various open-source projects financially and bringing their engineers 
-                 to help build a healthy open source community.
+                Google, Meta, are supporting various open-source projects financially and bringing their engineers 
+                to help build a healthy open source community.
                 </p>
 
                 <p>To contribute make a donation</p>
@@ -21,6 +25,7 @@ export default function Festival() {
                 <a href='https://opencollective.com/osca-kampala-chapter/donate'>
                 <Button>Donate</Button>
                 </a>
-            </>
+            </Container>
+
     )
 }
