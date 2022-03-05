599605560d4acbf829fda7c95366b5e76b8940f9
Abraham Jerry Kakooza <abrahamkakooza@gmail.com>
2022-03-05 22:48:13 +0300

    image work

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import * as React from 'react';
 import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
@@ -13,7 +12,6 @@ import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import YouTubeIcon from '@mui/icons-material/YouTube';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import { Button } from '@mui/material';
-import osca_logo from '../public/osca_logo.png'
 
 const Item = styled(Paper)(({ theme }) => ({
   ...theme.typography.body2,
