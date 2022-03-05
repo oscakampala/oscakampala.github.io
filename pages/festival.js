6760233f6c24ddf580fa0fd67ff912770d9ef2d4
Abraham Jerry Kakooza <abrahamkakooza@gmail.com>
2022-03-06 00:40:20 +0300

    Improve festival page

--- pages/festival.js
+++ pages/festival.js
@@ -6,13 +6,20 @@ export default function Festival() {
         <div className="container">
             <Container>
                 <h1>
-                    THE OPEN SOURCE FESTIVAL 2022 KAMPALA EDITION
+                    THE OPEN SOURCE FESTIVAL 2022, KAMPALA EDITION
                 </h1>
 
-                <h3>Date : 9th April 2022</h3>
-                <h4>Time : 10 AM to 5 PM</h4>
+                <h3>
+                    Date : 9th April 2022
+                </h3>
 
-                <h2>Concept note</h2>
+                <h4>
+                    Time : 10 AM to 5 PM
+                </h4>
+
+                <h2>
+                    Concept note
+                </h2>
                 
                 <p>Open collaboration often allows communities to achieve more than their members can do alone. 
                 This, in turn, accelerates innovation and is one of the reasons all the tech giants such as Microsoft,
@@ -21,20 +28,32 @@ export default function Festival() {
                 </p>
 
                 <a href='https://bit.ly/oscafestkla'>
-                <Button>RSVP</Button>
+                    <Button>
+                        R.S.V.P
+                    </Button>
                 </a>
 
                 <p>You can contribute by making a donation</p>
 
 
                 <a href='https://opencollective.com/osca-kampala-chapter/donate'>
-                <Button>Donate</Button>
+                    <Button>
+                        Donate
+                    </Button>
                 </a>
 
-                <h6>Event Sponsors</h6>
+                <h6>
+                    Event Sponsors
+                </h6>
 
                 <img width={70} height={70} src="/sponsors/TUNGA.png" alt="TUNGA Logo" />
-                
+
+                <img width={70} height={70} src="/osca-logo.png" alt="OSCA Logo" />
+
+                <h6>
+                    Open Source Community Africa (OSCA) is a community for open source lovers, enthusiasts, advocates and experts within and across Africa, with the sole aim of increasing the rate of credible open source contributions by Africans.
+                </h6>
+
             </Container>
             
 
