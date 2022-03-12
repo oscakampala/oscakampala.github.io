2a25fd95752e478348ef8e9444aadadbf1149b72
Piuskevin3 <mpavatarpius@gmail.com>
2022-03-13 00:59:43 +0300

    bootstrap fix

---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+import 'bootstrap/dist/css/bootstrap.css'; 
+//import '../styles/globals.css'
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
