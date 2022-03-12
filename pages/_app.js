8dc8c2ac535a4e1ead10364e3831b530613eafbb
Piuskevin3 <mpavatarpius@gmail.com>
2022-03-13 01:02:38 +0300

    bootstrap fix updates

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
+import {useEffect} from 'react';
 import 'bootstrap/dist/css/bootstrap.css'; 
 //import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
+
+    useEffect(() => {
+        import("bootstrap/dist/js/bootstrap");
+    }, []);
+
   return <Component {...pageProps} />
 }
 
