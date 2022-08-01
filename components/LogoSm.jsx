3f5db12f178f1dc1ec7e1c84e0888b9b08756c44
Marshud <ubatamye@gmail.com>
2022-08-02 01:44:41 +0300

    Footer and title edits

--- components/LogoSm.jsx
+++ components/LogoSm.jsx
@@ -5,11 +5,14 @@ import Image from "next/image";
 function LogoSm() {
   return (
     <div className="flex items-center text-[10px]">
-      <Image src="/images/osca-logo.png" height={60} width={60} />
-      <div className="logoWords">
-        Open Source Community Africa {"{"}{" "}
-        <span className="text-orange-400">Kampala</span>
-        {" }"}
+      <Image src="/images/osca-logo.png" height={30} width={30} />
+      <div className="logoWords w-96">
+        <p className="text-xs">
+          Open Source Community Africa {"{"}{" "}
+          <span className="text-orange-400">Kampala</span>
+          {" }"}
+        </p>
+        <p>We are a first growing Open Source Community based in Uganda, but an extension of Open Source Community Africa</p>
       </div>
     </div>
   );
