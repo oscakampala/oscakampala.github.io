91b1f20c376c070fb9d8d92bc61d1e90ddd4b9da
asumanwebdev <asumanwebdev@gmail.com>
2022-08-17 18:09:09 +0300

    created a separate component for the desktop navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -30,42 +30,9 @@ function Navbar() {
     setNav(!nav);
   };
 
-  const links = [
-    { text: 'about', path: '/about' },
-    { text: 'projects', path: '/projects' },
-    { text: 'experince', path: '/experience' },
-    { text: 'events', path: '/events' }
-  ];
-
   return (
-    <div className='fixed w-full h-20 shadow-xl z-10 bg-[#e5e7eb] mt-0 '>
-      <div className='flex justify-between items-center w-full h-full px-5 mx-auto border-2 border-[red]'>
-        <LogoLg />
-
-        <ul className='hidden md:flex items-center'>
-          {links.map((link, index) => {
-            const { text, path } = link;
-            return (
-              <Link href={path} key={index}>
-                <li className='capitalize text-xs lg:text-sm tracking-wider ml-10 font-bold'>
-                  {text}
-                </li>
-              </Link>
-            );
-          })}
-          <Link href='https://t.me/oscakampala'>
-            <button className='ml-10 text-xs lg:text-sm tracking-wider none shadow-md hover:shadow-xl normal-case text-white py-2 px-10 bg-[#C6C600] font-bold rounded-md'>
-              Join community
-            </button>
-          </Link>
-          <Link href='/login' passHref>
-            <button className='ml-10 text-xs lg:text-sm tracking-wider capitalize shadow-md hover:shadow-xl text-white py-2 px-10 bg-[#5A75AB] font-bold rounded-md'>
-              login
-            </button>
-          </Link>
-        </ul>
-      </div>
-
+    <div className='fixed w-full h-20 shadow-xl z-10 bg-[#e5e7eb] mt-0'>
+      <DeskTopNavbar />
       <div onClick={handleClick} className='md:hidden cursor-pointer '>
         <CgMenuRight size={30} />
       </div>
@@ -136,6 +103,45 @@ function Navbar() {
           </div>
         </div>
       </div>
+    </div>
+  );
+}
+
+const DeskTopNavbar = () => {
+  
+   const links = [
+     { text: 'about', path: '/about' },
+     { text: 'projects', path: '/projects' },
+     { text: 'experince', path: '/experience' },
+     { text: 'events', path: '/events' },
+   ];
+
+  return (
+    <div className='flex justify-between items-center w-full h-full px-5 mx-auto border-2 border-[red]'>
+      <LogoLg />
+
+      <ul className='hidden md:flex items-center'>
+        {links.map((link, index) => {
+          const { text, path } = link;
+          return (
+            <Link href={path} key={index}>
+              <li className='capitalize text-xs lg:text-sm tracking-wider ml-10 font-bold'>
+                {text}
+              </li>
+            </Link>
+          );
+        })}
+        <Link href='https://t.me/oscakampala'>
+          <button className='ml-10 text-xs lg:text-sm tracking-wider none shadow-md hover:shadow-xl normal-case text-white py-2 px-10 bg-[#C6C600] font-bold rounded-md'>
+            Join community
+          </button>
+        </Link>
+        <Link href='/login' passHref>
+          <button className='ml-10 text-xs lg:text-sm tracking-wider capitalize shadow-md hover:shadow-xl text-white py-2 px-10 bg-[#5A75AB] font-bold rounded-md'>
+            login
+          </button>
+        </Link>
+      </ul>
     </div>
   );
 }
