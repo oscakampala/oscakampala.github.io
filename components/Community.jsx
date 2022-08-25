98995c601450024e64c2d5bbe981516204afff4e
asumanwebdev <asumanwebdev@gmail.com>
2022-08-25 18:41:10 +0300

    design layout fro desktop and medium screens

--- components/Community.jsx
+++ components/Community.jsx
@@ -2,22 +2,24 @@ import oscaLogo from '../public/images/osca-logo.png';
 import Image from 'next/image';
 import { useState } from 'react';
 import { FaEye, FaEyeSlash } from 'react-icons/fa';
-import Alert from './Utils'
+import Alert from './Utils';
 
 const Community = () => {
   return (
-    <section>
-      <div className='flex mt-7 items-center cursor-pointer relative'>
+    <section className='max-w-6xl mx-auto shadow-sm '>
+      <div className='flex mt-7 md:mt-16 items-center cursor-pointer relative'>
         <div className='md:absolute left-0 flex items-center'>
           <Image src={oscaLogo} width={132} height={130} />
         </div>
-        <p className='md:ml-20 text-2xl font-bold'>
+        <p className='md:ml-36 text-2xl font-bold'>
           OSCA{' { '}
           <span className='text-secondary-4 '>Kampala</span>
           {' }'}
         </p>
       </div>
-      <h1 className='px-5 text-3xl'>Join a Large Growing Community in Town</h1>
+      <h1 className='px-5 text-3xl md:mt-10'>
+        Join a Large Growing Community in Town
+      </h1>
       <p className='px-5 text-2xl pt-7'>
         This content is private (for you only)
       </p>
@@ -41,38 +43,50 @@ const Form = () => {
     msg: '',
   });
 
-  const inputStyles = `rounded-md bg-[D9D9D9] py-2 pl-2 font-medium mt-4 w-full outline-none`;
-
   const user = {
     firstname,
     lastname,
     email,
     username,
     password,
-    title
-  }
+    title,
+  };
 
- const handleSubmit = (e) => {
-   e.preventDefault();
-   console.log('User details: ', user);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('User details: ', user);
 
-   if (password !== retypedPassword) {
+    const reg =
+      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{3,}))$/;
+
+    if (!reg.test(email)) {
+      setAlert({
+        type: 'danger',
+        msg: 'Please enter a valid email address',
+        show: true,
+      });
+      return;
+    }
+
+    if (password !== retypedPassword) {
       setAlert({
         show: true,
         type: 'danger',
         msg: 'Passwords do not match',
       });
-     return;
+      return;
     }
 
-   setEmail('');
-   setFirstname('');
-   setlastname('');
-   setUsername('');
-   setPassword('');
-   setRetypedPassword('');
-   setTitle('')
- }
+    setEmail('');
+    setFirstname('');
+    setlastname('');
+    setUsername('');
+    setPassword('');
+    setRetypedPassword('');
+    setTitle('');
+  };
+
+  const inputStyles = `rounded-md bg-[D9D9D9] py-2 pl-2 font-medium mt-4 w-full outline-none`;
 
   return (
     <div>
@@ -82,30 +96,32 @@ const Form = () => {
             {alert.show && <Alert alert={alert} setAlert={setAlert} />}
           </div>
         </div>
-        <div>
-          <h3 className='text-black'>First Name:*</h3>
-          <input
-            type='text'
-            className={inputStyles}
-            value={firstname}
-            onChange={(e) => setFirstname(e.target.value)}
-            required
-          />
-        </div>
-        <div className='mt-6'>
-          <h3 className='text-black'>Last Name:</h3>
-          <input
-            type='text'
-            className={inputStyles}
-            value={lastname}
-            onChange={(e) => setlastname(e.target.value)}
-          />
+        <div className='md:flex w-full justify-between'>
+          <div className='md:w-[50%]'>
+            <h3 className='text-black'>First Name:*</h3>
+            <input
+              type='text'
+              className={inputStyles}
+              value={firstname}
+              onChange={(e) => setFirstname(e.target.value)}
+              required
+            />
+          </div>
+          <div className='mt-6 md:mt-0 md:w-[50%] md:ml-8'>
+            <h3 className='text-black'>Last Name:</h3>
+            <input
+              type='text'
+              className={inputStyles}
+              value={lastname}
+              onChange={(e) => setlastname(e.target.value)}
+            />
+          </div>
         </div>
         <div className='mt-6'>
           <h3 className='text-black'>Email:*</h3>
           <input
             type='text'
-            className={inputStyles}
+            className={`${inputStyles} md:w-[48%]`}
             value={email}
             onChange={(e) => setEmail(e.target.value)}
             required
@@ -115,7 +131,7 @@ const Form = () => {
           <h3 className='text-black'>Username:*</h3>
           <input
             type='text'
-            className={inputStyles}
+            className={`${inputStyles} md:w-[48%]`}
             value={username}
             onChange={(e) => setUsername(e.target.value)}
             required
@@ -125,7 +141,7 @@ const Form = () => {
           <h3 className='text-black'>Password:*</h3>
           <input
             type={showPassword ? 'text' : 'password'}
-            className={inputStyles}
+            className={`${inputStyles} md:w-[48%]`}
             value={password}
             onChange={(e) => setPassword(e.target.value)}
             required
@@ -134,7 +150,7 @@ const Form = () => {
             <FaEye
               id='password-eye'
               onClick={() => setShowPassword(!showPassword)}
-              className='absolute bottom-2 right-1 lg:cursor-pointer'
+              className='absolute bottom-2 right-1 md:right-[53%] lg:cursor-pointer'
             />
           ) : (
             <FaEyeSlash
@@ -147,45 +163,68 @@ const Form = () => {
         <div className='mt-6'>
           <h3 className='text-black'>Retype Password:*</h3>
           <input
-            type='text'
-            className={inputStyles}
+            type='password'
+            className={`${inputStyles} md:w-[48%]`}
             value={retypedPassword}
             onChange={(e) => setRetypedPassword(e.target.value)}
             required
           />
         </div>
-        <div className='mt-6'>
-          <h3 className='text-black'>Title:*</h3>
+        <div className='mt-6 md:flex items-center'>
+          <h3 className='text-black md:mr-12'>Title:*</h3>
           <div
-            className='flex items-center mt-3'
+            className='flex items-center mt-3 md:mt-0 md:mr-16 '
             onClick={() => setTitle('mr.')}>
-            <input type='radio' id='mr' name='title' className='w-5 h-5' />
-            <label htmlFor='mr' className='ml-4 text-lg font-bold'>
+            <input
+              type='radio'
+              id='mr'
+              name='title'
+              className='w-5 h-5 cursor-pointer'
+            />
+            <label
+              htmlFor='mr'
+              className='ml-4 text-lg font-bold cursor-pointer'>
               Mr.
             </label>
           </div>
           <div
-            className='flex items-center mt-3'
+            className='flex items-center mt-3 md:mt-0 md:mr-16 '
             onClick={() => setTitle('mrs')}>
-            <input type='radio' id='mrs' name='title' className='w-5 h-5' />
-            <label htmlFor='mrs' className='ml-4 text-lg font-bold'>
+            <input
+              type='radio'
+              id='mrs'
+              name='title'
+              className='w-5 h-5 cursor-pointer'
+            />
+            <label
+              htmlFor='mrs'
+              className='ml-4 text-lg font-bold cursor-pointer'>
               Mrs.
             </label>
           </div>
           <div
-            className='flex items-center mt-3'
+            className='flex items-center mt-3 md:mt-0'
             onClick={() => setTitle('others')}>
-            <input type='radio' id='others' name='title' className='w-5 h-5' />
-            <label htmlFor='others' className='ml-4 text-lg font-bold'>
+            <input
+              type='radio'
+              id='others'
+              name='title'
+              className='w-5 h-5 cursor-pointer'
+            />
+            <label
+              htmlFor='others'
+              className='ml-4 text-lg font-bold cursor-pointer'>
               Others
             </label>
           </div>
         </div>
-        <button
-          type='submit'
-          className='mt-12 w-full rounded-md py-3 capitalize bg-[#5A75AB] text-white font-bold tracking-wider'>
-          Submit
-        </button>
+        <div className='w-full flex justify-end'>
+          <button
+            type='submit'
+            className='mt-12 w-full md:w-36 rounded-md py-3 capitalize bg-[#5A75AB] text-white font-bold tracking-wider'>
+            Submit
+          </button>
+        </div>
       </form>
     </div>
   );
