2d40688dd627a7d8d0c3bedf2c9408f187845d18
asumanwebdev <asumanwebdev@gmail.com>
2022-08-25 16:51:14 +0300

    set states for input elements of community page

--- components/Community.jsx
+++ components/Community.jsx
@@ -9,15 +9,15 @@ const Community = () => {
     <section>
       <div className='flex mt-7 items-center cursor-pointer relative'>
         <div className='md:absolute left-0 flex items-center'>
-          <Image src={oscaLogo} width={80} height={80} />
+          <Image src={oscaLogo} width={132} height={130} />
         </div>
-        <p className='md:ml-20'>
+        <p className='md:ml-20 text-2xl font-bold'>
           OSCA{' { '}
           <span className='text-secondary-4 '>Kampala</span>
           {' }'}
         </p>
       </div>
-      <h1 className='px-5'>Join a Large Growing Community in Town</h1>
+      <h1 className='px-5 text-3xl'>Join a Large Growing Community in Town</h1>
       <p className='px-5 text-2xl pt-7'>
         This content is private (for you only)
       </p>
@@ -31,10 +31,10 @@ const Form = () => {
   const [email, setEmail] = useState('');
   const [firstname, setFirstname] = useState('');
   const [lastname, setlastname] = useState('');
+  const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [retypedPassword, setRetypedPassword] = useState('');
   const [title, setTitle] = useState('');
-  
 
   const inputStyles = `rounded-md bg-[D9D9D9] py-2 pl-2 font-medium mt-4 w-full outline-none`;
   return (
@@ -42,21 +42,47 @@ const Form = () => {
       <form action='' className='mt-12 px-5'>
         <div>
           <h3 className='text-black'>First Name:</h3>
-          <input type='text' className={inputStyles} value={firstname} onChange/>
-        </div>
-        <div className='mt-6'>
-          <h3 className='text-black'>Username:</h3>
-          <input type='text' className={inputStyles} />
+          <input
+            type='text'
+            className={inputStyles}
+            value={firstname}
+            onChange={(e) => setFirstname(e.target.value)}
+          />
         </div>
         <div className='mt-6'>
           <h3 className='text-black'>Last Name:</h3>
-          <input type='text' className={inputStyles} />
+          <input
+            type='text'
+            className={inputStyles}
+            value={lastname}
+            onChange={(e) => setlastname(e.target.value)}
+          />
+        </div>
+        <div className='mt-6'>
+          <h3 className='text-black'>Email:</h3>
+          <input
+            type='text'
+            className={inputStyles}
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className='mt-6'>
+          <h3 className='text-black'>Username:</h3>
+          <input
+            type='text'
+            className={inputStyles}
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
         </div>
         <div className='mt-6 relative'>
           <h3 className='text-black'>Password:</h3>
           <input
             type={showPassword ? 'text' : 'password'}
             className={inputStyles}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
           {showPassword ? (
             <FaEye
@@ -74,21 +100,38 @@ const Form = () => {
         </div>
         <div className='mt-6'>
           <h3 className='text-black'>Retype Password:</h3>
-          <input type='text' className={inputStyles} />
+          <input
+            type='text'
+            className={inputStyles}
+            value={retypedPassword}
+            onChange={(e) => setRetypedPassword(e.target.value)}
+          />
         </div>
         <div className='mt-6'>
           <h3 className='text-black'>Title:</h3>
-          <div className='flex items-center mt-3'>
-            <input type="radio" id='mr' name='title' className='w-5 h-5' />
-            <label htmlFor="mr" className='ml-4 text-lg font-bold'>Mr.</label>
+          <div
+            className='flex items-center mt-3'
+            onClick={() => setTitle('mr.')}>
+            <input type='radio' id='mr' name='title' className='w-5 h-5' />
+            <label htmlFor='mr' className='ml-4 text-lg font-bold'>
+              Mr.
+            </label>
           </div>
-          <div className='flex items-center mt-3'>
-            <input type="radio" id='mrs' name='title' className='w-5 h-5' />
-            <label htmlFor="mrs" className='ml-4 text-lg font-bold'>Mrs.</label>
+          <div
+            className='flex items-center mt-3'
+            onClick={() => setTitle('mrs')}>
+            <input type='radio' id='mrs' name='title' className='w-5 h-5' />
+            <label htmlFor='mrs' className='ml-4 text-lg font-bold'>
+              Mrs.
+            </label>
           </div>
-          <div className='flex items-center mt-3'>
-            <input type="radio" id='others' name='title' className='w-5 h-5' />
-            <label htmlFor="others" className='ml-4 text-lg font-bold'>Others</label>
+          <div
+            className='flex items-center mt-3'
+            onClick={() => setTitle('others')}>
+            <input type='radio' id='others' name='title' className='w-5 h-5' />
+            <label htmlFor='others' className='ml-4 text-lg font-bold'>
+              Others
+            </label>
           </div>
         </div>
       </form>
