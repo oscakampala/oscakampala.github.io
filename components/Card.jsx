4b50ba612581aad6e80299400aa57787d6207c87
Nova <80816415+novalabsug@users.noreply.github.com>
2023-05-04 21:27:59 +0300

    Change deprecated package react-reveal to react-awesome-reveal

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,9 +1,10 @@
 /** @format */
 
-import Image from 'next/image';
-import { FaLinkedin, FaTwitter, FaGithub } from 'react-icons/fa';
-import Zoom from 'react-reveal/Zoom';
-import Fade from 'react-reveal/Fade';
+import Image from "next/image";
+import { FaLinkedin, FaTwitter, FaGithub } from "react-icons/fa";
+// import Zoom from 'react-reveal/Zoom';
+// import Fade from 'react-reveal/Fade';
+import { Zoom, Fade } from "react-awesome-reveal";
 
 function Card({
   url,
@@ -14,41 +15,40 @@ function Card({
   leadTwitter,
   leadGithub,
 }) {
- 
   return (
-    <div className='flex flex-col w-full justify-center shadow shadow-black-500/40 p-8 rounded bg-slate-200'>
-      <Zoom bottom>
+    <div className="flex flex-col w-full justify-center shadow shadow-black-500/40 p-8 rounded bg-slate-200">
+      <Zoom triggerOnce={true}>
         <Image
           src={leadImg}
-          objectFit='cover'
-          className='rounded'  
+          objectFit="cover"
+          className="rounded"
           height={600}
           width={500}
-        />{' '}
+        />{" "}
       </Zoom>
-      <div className='flex sm:flex-col md:flex-row justify-between container px-2 pt-4 '>
-        <Fade right>
+      <div className="flex sm:flex-col md:flex-row justify-between container px-2 pt-4 ">
+        <Fade triggerOnce={true} direction="right">
           <div>
             <h3>{leadName}</h3>
-            <p className='banner'>{leadLang}</p>
+            <p className="banner">{leadLang}</p>
           </div>
         </Fade>
 
-        <Fade left>
-          <div className='flex gap-2 sm:mt-3 md:mt-0'>
-            <div className='cursor-pointer'>
-              {' '}
-              <a target='_blank' href={leadLinkedin}>
+        <Fade triggerOnce={true} direction="left">
+          <div className="flex gap-2 sm:mt-3 md:mt-0">
+            <div className="cursor-pointer">
+              {" "}
+              <a target="_blank" href={leadLinkedin}>
                 <FaLinkedin size={20} />
               </a>
             </div>
-            <div className='cursor-pointer'>
-              <a target='_blank' href={leadTwitter}>
+            <div className="cursor-pointer">
+              <a target="_blank" href={leadTwitter}>
                 <FaTwitter size={20} />
               </a>
             </div>
-            <div className='cursor-pointer'>
-              <a target='_blank' href={leadGithub}>
+            <div className="cursor-pointer">
+              <a target="_blank" href={leadGithub}>
                 <FaGithub size={20} />
               </a>
             </div>
